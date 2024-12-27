f85f64145edeab69803138635f01159d9cdbdd90
IsaacEi <12915389+IsaacEi@users.noreply.github.com>
2024-12-26 19:25:06 -0600

    reportarMiMascota

--- src/controllers/reporte.controller.ts
+++ src/controllers/reporte.controller.ts
@@ -1,17 +1,18 @@
 import { Request, Response } from 'express';
 import { storeProcedure } from '../classes/database';
 
-// Reportar mi mascota
+// Reportar mi mascota (FALTA POR TERMINAR PSAUMIS)
 export async function reportarMiMascota(req: any, res: Response): Promise<Response> {
     try {
         const { id } = req.usuario;
-        const { mascota, latitud, longitud } = req.body;
+        const { mascota, latitud, longitud, tipo } = req.body;
         const body = {
             storeProcedure: 'reportarMiMascota',
             vusuario: id,
             vmascota: mascota,
             vlatitud: latitud,
             vlongitud: longitud,
+            vtipo: tipo
         }; 
         const sp = await storeProcedure(body);
         const data = sp[0][0];
